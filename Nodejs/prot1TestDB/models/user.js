692d8b4dbea569ab624ff2faa837beefea123ad3
pdh4182@gmail.com <Rlatmdwl12!>
2022-06-24 05:00:42 +0900

    Node Server origin

---
+++ Nodejs/prot1TestDB/models/user.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      id: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+        primaryKey: true,
+      },
+      nick:{
+        type: Sequelize.STRING(15),
+        allowNull: false,
+        primaryKey: true,
+      },
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      }
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'User',
+      tableName: 'users',
+      paranoid: true,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  }
+
+  //static associate(db) {
+  //나중에 추가할수도
+  //}
+};
---
+++ Nodejs/prot1TestDB/routes/auth.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/join', isNotLoggedIn, async (req, res, next) => {
+  const { id, nick, password } = req.body;
+    try {
+        const exUser = await User.findOne({ where: { id } });
+        if (exUser) {
+        return res.redirect('/join?error=exist');
+        }
+        const hash = await bcrypt.hash(password, 12);
+        await User.create({
+        id,
+        password: hash,
+        nick,
+        });
+        return res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
+});
+
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+  passport.authenticate('local', (authError, user, info) => {
+    if (authError) {
+      console.error(authError);
+      return next(authError);
+    }
+    if (!user) {
+      return res.redirect(`/?loginError=${info.message}`);
+    }
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      return res.redirect('/');
+    });
+  })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙입니다.
+});
+
+//회원탈퇴
+router.get('/delUser/:userid', isLoggedIn, async (req, res) => {
+  const userid = req.params.userid;
+  try {
+    const exUser = await User.findOne({ where: { id : userid } });
+    if (exUser) {
+      await User.destroy({
+        where:{
+          id: userid
+        }
+      })
+      return res.redirect('/');   
+    }
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+});
+//회원탈퇴
+
+router.get('/logout', isLoggedIn, (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+});
+
+
+module.exports = router;
