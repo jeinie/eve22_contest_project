763ca6661dbb30def1e3b53e295571ca55c26e66
장정윤 <jeinie9179@naver.com>
2022-07-20 23:18:28 +0900

    버스검색 api

---
+++ Sample2/clientApp/app/src/main/java/com/example/testapp/data/MsgBody.kt
@@ -0,0 +1,5 @@
+package com.example.testapp.data
+
+data class MsgBody(
+    val itemList: List<ItemList>
+)
---
+++ Sample2/clientApp/app/src/main/java/com/example/testapp/data/MsgHeader.kt
@@ -0,0 +1,7 @@
+package com.example.testapp.data
+
+data class MsgHeader(
+    val headerCd: String,
+    val headerMsg: String,
+    val itemCount: Int
+)
