bd6fbe9b8784f53632b7a202c266d325d28aa8f9
DHAPARK <pdh4182@gmail.com>
2022-07-28 22:53:27 +0900

    0728작업

--- KotlinClient/clientApp/app/src/main/java/com/example/testapp/activity/BusesAtStationAcitivity.kt
+++ KotlinClient/clientApp/app/src/main/java/com/example/testapp/activity/BusesAtStationAcitivity.kt
@@ -1,22 +1,23 @@
 package com.example.testapp.activity
 
+import android.content.DialogInterface
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.os.StrictMode
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testapp.Adapter.BusesAtStationCustomeAdapter
-import com.example.testapp.Adapter.StationsCustomAdapter
+
 import com.example.testapp.Model.ArriveBusInfo.ArriveBusInfo
-import com.example.testapp.Model.RouteModel.RouteModel
-import com.example.testapp.Model.StationModel.StationModel
 import com.example.testapp.controller.BusesAtStationController.BusesAtStationController
 import com.example.testapp.databinding.ActivityBusListAtStationBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 class BusesAtStationAcitivity : AppCompatActivity() , CoroutineScope {
@@ -28,11 +29,14 @@ class BusesAtStationAcitivity : AppCompatActivity() , CoroutineScope {
     lateinit var binding : ActivityBusListAtStationBinding
     lateinit var basc : BusesAtStationController
     lateinit var basca : BusesAtStationCustomeAdapter
+
+    //
+    lateinit var DialogBuilder : AlertDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         job = Job()
-
 
         window.statusBarColor = Color.parseColor("#90008000")
         binding = ActivityBusListAtStationBinding.inflate(layoutInflater)
@@ -44,6 +48,8 @@ class BusesAtStationAcitivity : AppCompatActivity() , CoroutineScope {
             val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
             StrictMode.setThreadPolicy(policy)
         }
+
+        val DialogBuilder = AlertDialog.Builder(this)
 
 
         val stId = intent.getStringExtra("stId")
@@ -77,7 +83,30 @@ class BusesAtStationAcitivity : AppCompatActivity() , CoroutineScope {
                 //sc.loadBus(model.routeId,applicationContext)
 
                 //basc.getArriveBus(model.stationId,routeId!!,model.ord,routeNum!!,model.stationNm,applicationContext)
-
+                //여기서 클릭하면 나오면 좋겠는데
+                //여기부터 다이얼로그
+                //model.vehId
+                DialogBuilder
+                    .setTitle("${stationNm} ( ${model.routeNum} )")
+                    .setMessage("${model.arrmsg}")
+                //메세지만 띄워준상태고
+                var DialogBuilderListener = object : DialogInterface.OnClickListener {
+                    override fun onClick(p0: DialogInterface?, p1: Int) {
+                        when (p1) {
+                            DialogInterface.BUTTON_POSITIVE ->
+                                Toast.makeText(baseContext,"예약되었습니다.",Toast.LENGTH_SHORT).show()
+                            DialogInterface.BUTTON_NEGATIVE ->
+                                Log.d("그냥취소","그냥취소")
+                            DialogInterface.BUTTON_NEUTRAL ->
+                                Log.d("예약취소","예약취소")
+                        }
+                    }
+                }
+                DialogBuilder.setPositiveButton("예약하기",DialogBuilderListener)
+                DialogBuilder.setNegativeButton("취소",DialogBuilderListener)
+                DialogBuilder.setNeutralButton("예약취소",DialogBuilderListener)
+                DialogBuilder.show()
+                //여기까지 다이얼로그
             }
         })
         Log.d("여기 오긴하나","오긴하나")
--- KotlinClient/clientApp/app/src/main/java/com/example/testapp/controller/BusesAtStationController/BusesAtStationController.kt
+++ KotlinClient/clientApp/app/src/main/java/com/example/testapp/controller/BusesAtStationController/BusesAtStationController.kt
@@ -1,4 +1,9 @@
 package com.example.testapp.controller.BusesAtStationController
 
+import android.content.Context
+import android.content.DialogInterface
+import androidx.appcompat.app.AlertDialog
+
 class BusesAtStationController {
+
 }
