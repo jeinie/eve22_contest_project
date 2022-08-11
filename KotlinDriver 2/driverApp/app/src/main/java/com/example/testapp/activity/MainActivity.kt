2877ca5997f4426f64c7897121e2e6a88cab233c
DHAPARK <pdh4182@gmail.com>
2022-08-11 17:03:10 +0900

    안드로이드폰구해야함

--- KotlinDriver 2/driverApp/app/src/main/java/com/example/testapp/activity/MainActivity.kt
+++ KotlinDriver 2/driverApp/app/src/main/java/com/example/testapp/activity/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.testapp.activity
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebViewClient
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testapp.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
@@ -15,15 +19,51 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(busNum: String?): Boolean {
-                Log.d("확인", "입력한 버스넘버 : $busNum")
-                return true
-            }
 
-            override fun onQueryTextChange(p0: String?): Boolean {
-                return true
+
+        // QR 코드 버튼 이벤트
+        binding.button.setOnClickListener {
+
+            val integrator = IntentIntegrator(this)
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE) // 여러가지 바코드중에 특정 바코드 설정 가능
+            integrator.setPrompt("QR 코드를 스캔하여 주세요:)") // 스캔할 때 하단의 문구
+            integrator.setCameraId(0) // 0은 후면 카메라, 1은 전면 카메라
+            integrator.setBeepEnabled(true) // 바코드를 인식했을 때 삑 소리유무
+            integrator.setBarcodeImageEnabled(false) // 스캔 했을 때 스캔한 이미지 사용여부
+            integrator.initiateScan() // 스캔
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        // QR 코드를 찍은 결과를 변수에 담는다.
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        Log.d("TTT", "QR 코드 체크")
+
+        //결과가 있으면
+        if (result != null) {
+            // 컨텐츠가 없으면
+            if (result.contents == null) {
+                //토스트를 띄운다.
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
             }
-        })
+            // 컨텐츠가 있으면
+            else {
+                //토스트를 띄운다.
+                Toast.makeText(this, "scanned" + result.contents, Toast.LENGTH_LONG).show()
+                Log.d("TTT", "QR 코드 URL:${result.contents}")
+
+                //웹뷰 설정
+                binding.webView.settings.javaScriptEnabled = true
+                binding.webView.webViewClient = WebViewClient()
+
+                //웹뷰를 띄운다.
+                binding.webView.loadUrl(result.contents)
+            }
+            // 결과가 없으면
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
