ae7de488d3f6bcb8b8579bf059cfc26a25b9b300
장정윤 <jeinie9179@naver.com>
2022-11-18 19:55:18 +0900

    Realtime Database 파이썬 연결

---
+++ Python/pyrebase.py
@@ -0,0 +1,13 @@
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+db_url = 'https://eveproject-624f6-default-rtdb.asia-southeast1.firebasedatabase.app/'
+cred = credentials.Certificate("eveproject-624f6-firebase-adminsdk-uugft-84202cf8b1.json")
+default_app = firebase_admin.initialize_app(cred, {
+    'databaseURL': db_url
+})
+
+ref = db.reference() # 기본 위치 지
+ref.push({'message':'Hello'})
