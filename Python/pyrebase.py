be810a71050aa2fc9fd2dac8a7beef06fb5a6c10
장정윤 <jeinie9179@naver.com>
2022-11-18 20:33:29 +0900

    .gitignore

--- Python/pyrebase.py
+++ Python/pyrebase.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 db_url = 'https://eveproject-624f6-default-rtdb.asia-southeast1.firebasedatabase.app/'
-cred = credentials.Certificate("eveproject-624f6-firebase-adminsdk-uugft-84202cf8b1.json")
+cred = credentials.Certificate("config.json")
 default_app = firebase_admin.initialize_app(cred, {
     'databaseURL': db_url
 })
